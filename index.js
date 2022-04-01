384ccf14267f487bcc996a8f447a5f6cdd720fd5
Sanderson Srisouraj <ssanderson295@gmail.com>
2022-04-01 14:25:56 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, string) {
+    return drivers.filter(name => {
+        return name.toLowerCase() === string.toLowerCase();
+    })
+}
+
+function fuzzyMatch(driver, string) {
+    return driver.filter(name => {
+        return name.slice(0, string.length) === string;
+    });
+}
+
+function matchName(driver, string) {
+    return driver.filter(function (driver) {
+        return driver.name === string;
+    })
+}
